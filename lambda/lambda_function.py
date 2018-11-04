18ee3c113acae4f19cda21ef5c9998738d76e405
Matthew Hanson <matt.a.hanson@gmail.com>
2018-11-04 01:01:08 -0400

    add initial lambda function

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,11 +1,12 @@
 import logging
+import sys
 
 from satstac.landsat import transform
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 
-def lambda_handler(event):
+def lambda_handler(event, context):
     logger.info(event)
-    
-    
+    logger.info(context)
