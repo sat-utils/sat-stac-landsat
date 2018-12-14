a5eb3f6ee77f3d9474fb8a1420f12b50a64ea287
Matthew Hanson <matt.a.hanson@gmail.com>
2018-12-14 00:14:51 -0500

    fix logging error

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -32,7 +32,7 @@ def lambda_handler(event, context):
         }
         # transform to STAC
         item = transform(data)
-        logger.debug('Item: %s' % json.dumps(item))
+        logger.debug('Item: %s' % json.dumps(item.data))
         if item['landsat:tier'] != 'RT':
             collection.add_item(item, path='${eo:column}/${eo:row}/${date}')
             logger.info('Added %s as %s' % (item, item.filename))
